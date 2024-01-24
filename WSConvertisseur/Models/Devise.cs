543b668578fa649147f6882ad15e31c225cf3134
martmate <martmate@D360-S31>
2024-01-24 14:49:15 +0100

    ajout modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,42 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomDevise;
+        private double taux;
+
+        public int Id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        public string? NomDevise
+        {
+            get { return NomDevise; }
+            set { NomDevise = value; }
+        }
+        public double Taux
+        {
+            get { return Taux; }
+            set { Taux = value; }
+        }
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            this.Id = id;
+            this.NomDevise = nomDevise;
+            this.Taux = taux;
+        }
+        public Devise()
+        {
+
+
+
+
+        }
+
+
+
+
+    }
+}
